4ca9cdef74b2c5b3afa927dc4d9a3ce282b244ae
pepsikat <piratka-mc@mail.ru>
2015-11-15 22:43:52 +0500

    add layout

---
+++ config.rb
@@ -0,0 +1,18 @@
+Encoding.default_external = "UTF-8"
+
+require "sass-timestamp"
+
+http_path = "../"
+css_dir = "css"
+sass_dir = "scss"
+#images_dir = "img"
+javascripts_dir = "js"
+#fonts_dir = "fonts"
+
+output_style = :compressed
+
+relative_assets = true
+
+color_output = false
+
+preferred_syntax = :scss
